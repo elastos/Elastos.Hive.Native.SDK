f0f6579f4b8179679887bc17d4cf51abe287267c
Leng Bo <lengbo@storswift.com>
2019-04-12 16:09:26 +0800

    Refine testing code. (#15)

--- cpp-test/dstore_test.sh
+++ cpp-test/dstore_test.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 # Written by Wang Yi (<wangyi@storswift.com>), Wan Lei (<wanlei@storswift.com>), and Leng Bo (<lengbo@storswift.com>).
 
-# Uncomment the two lines to use the fixed UID
-# HIVE_UID=uid-ac27da17-5649-497e-a8a8-5f022b7f8f68
-# HIVE_PEERID=QmNTWTjUfe1AXkMpeTf47taXCWrfC6GhThmMCXmTgJybg5
+# Uncomment the two lines to use your own fixed UID
+# HIVE_UID=uid-f409f8e4-667f-4f55-9f4c-3b129375a663
+# HIVE_PEERID=QmZP7ahpnbyUyhToA2h1rJB6M5VXYcqscye8n1Rpr3ZN3b
 
+# You can set the log filenames. If not, the script will generate them.
+# logfile=dstore_test.log
+# statisticsfile=dstore_test.sta
 
-if [ $# -lt 2 ]; then
-    echo "Please set the IP addresses of two servers!"
-    echo " $0 SERVER1 SERVER2"
-    echo "Example:"
-    echo " $0 192.168.0.2 192.168.0.3"
-    echo "    # It will test send key-value to 192.168.0.2, and read this key-value from 192.168.0.3."
-    exit 1
+if [ "${logfile}" = "" ]; then
+    logfile=SERVER-$1-`date "+%Y-%m-%d_%H:%M"`.log
 fi
-
-HIVE_HOST1=$1
-HIVE_HOST2=$2
-
-if [ "${HIVE_UID}" = "" ] && [ "${HIVE_PEERID}" = "" ]; then
-    temp=`curl http://${HIVE_HOST1}:9095/api/v0/uid/new 2>/dev/null` \
-        || show_error "Can not new a UID on ${HIVE_HOST1}"
-    HIVE_UID=`echo ${temp} | jq .UID | sed s/\"//g`
-    HIVE_PEERID=`echo ${temp} | jq .PeerID | sed s/\"//g`
+if [ "${statisticsfile}" = "" ]; then
+    statisticsfile=SERVER-$1-`date "+%Y-%m-%d_%H:%M"`.sta
 fi
-
-logfile=SERVER-$1-`date "+%Y-%m-%d_%H:%M"`.log
-statisticsfile=SERVER-$1-`date "+%Y-%m-%d_%H:%M"`.sta
 
 function show_error()
 {
@@ -94,6 +82,24 @@ function msg_remove {
     dstore_test remove ${host} ${uid} ${key}
 }
 
+if [ $# -lt 2 ]; then
+    echo "Please set the IP addresses of two servers!"
+    echo " $0 SERVER1 SERVER2"
+    echo "Example:"
+    echo " $0 192.168.0.2 192.168.0.3"
+    echo "    # It will test send key-value to 192.168.0.2, and read this key-value from 192.168.0.3."
+    exit 1
+fi
+
+HIVE_HOST1=$1
+HIVE_HOST2=$2
+
+if [ "${HIVE_UID}" = "" ] && [ "${HIVE_PEERID}" = "" ]; then
+    temp=`curl http://${HIVE_HOST1}:9095/api/v0/uid/new 2>/dev/null` \
+        || show_error "Can not new a UID on ${HIVE_HOST1}"
+    HIVE_UID=`echo ${temp} | jq .UID | sed s/\"//g`
+    HIVE_PEERID=`echo ${temp} | jq .PeerID | sed s/\"//g`
+fi
 
 echo "===============================================================================================" \
     | tee -a ${logfile} ${statisticsfile}
@@ -102,11 +108,16 @@ echo "******************** Send key-value to ${HIVE_HOST1}. Read from ${HIVE_HOS
 echo "===============================================================================================" \
     | tee -a ${logfile} ${statisticsfile}
 echo 
-echo HIVE_UID: $HIVE_UID
-echo HIVE_PEERID: $HIVE_PEERID
+echo HIVE_UID=${HIVE_UID}
+echo HIVE_PEERID=${HIVE_PEERID}
 logs "[Info] Begin to  send key-value to ${HIVE_HOST1}"
+
+
+TEST_KEY=test-key-${HIVE_HOST2}
+TEST_VALUE=test-key-${HIVE_HOST2}-item001
+
 time_start_send=`date +%s.%N`
-msg_send ${HIVE_HOST1} ${HIVE_UID} "test-key-${HIVE_HOST2}" "test-key-${HIVE_HOST2}-item001"  \
+msg_send ${HIVE_HOST1} ${HIVE_UID} ${TEST_KEY} ${TEST_VALUE}  \
     || show_error "Cannot send key-value to ${HIVE_HOST1}."
 # sleep 3
 time_end_send=`date +%s.%N`
@@ -115,20 +126,26 @@ i=1
 res=""
 while :
 do
-    res=`msg_read ${HIVE_HOST2} ${HIVE_PEERID} "test-key-${HIVE_HOST2}"`
-    res3=`echo $res | awk '{print $7}'`
-    if [ x"$res3" == x"test-key-${HIVE_HOST2}-item001" ];then
+    res=`msg_read ${HIVE_HOST2} ${HIVE_PEERID} ${TEST_KEY}`
+    echo "****************************************"
+    echo "[Info] Key:${TEST_KEY}, Raw Value:"
+    echo ${res}
+    echo "****************************************"
+    res3=`echo ${res} | awk '{print $7}'`
+    if [ x"${res3}" = x"${TEST_VALUE}" ];then
         time_end_read=`date +%s.%N`
         logs "[Info] Now read key-value from ${HIVE_HOST2} successfully." 
-    break;
+        break
     fi
-    echo $i,
+    echo "[Info] Key:${TEST_KEY}, Value: ${res3}. But the value should be ${TEST_VALUE}. Wait 1 second."
+    sleep 1
     ((i++))
 done
-msg_remove ${HIVE_HOST1} ${HIVE_UID} "test-key-${HIVE_HOST2}"
+msg_remove ${HIVE_HOST1} ${HIVE_UID} ${TEST_KEY}
 logs_time Send_K-V_To_${HIVE_HOST1} ${time_start_send} ${time_end_send}
 logs_time Read_K-V_From_${HIVE_HOST2} ${time_end_send} ${time_end_read}
 echo "" >>$statisticsfile 
 
+sleep 3
 
 echo "Test done!"
