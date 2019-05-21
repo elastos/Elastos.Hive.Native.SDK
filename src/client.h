505fdf9e560590bda6ca4c7532b7fc613337a60e
lifenxiang <noahli2013@outlook.com>
2019-05-21 12:42:04 +0800

    merge client and drive apis into elastos_hive.h

--- src/client.h
+++ src/client.h
@@ -1,105 +1,21 @@
-#ifndef __HIVE_CLIENT_H__
-#define __HIVE_CLIENT_H__
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
 
-#if defined(__APPLE__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdocumentation"
-#endif
+#include "elastos_hive.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+struct HiveClient {
+    int (*login)(HiveClient *);
+    int (*logout)(HiveClient *);
+    int (*list_drives)(HiveClient *, char **);
+    HiveDrive *(*drive_open)(HiveClient *, const HiveDriveOptions *);
+    void (*destructor_func)(HiveClient *);
 
-#include "drive.h"
-
-#if defined(HIVE_STATIC)
-  #define HIVE_API
-#elif defined(HIVE_DYNAMIC)
-  #ifdef HIVE_BUILD
-    #if defined(_WIN32) || defined(_WIN64)
-      #define HIVE_API __declspec(dllexport)
-    #else
-      #define HIVE_API __attribute__((visibility("default")))
-    #endif
-  #else
-    #if defined(_WIN32) || defined(_WIN64)
-      #define HIVE_API __declspec(dllimport)
-    #else
-      #define HIVE_API
-    #endif
-  #endif
-#else
-  #define HIVE_API
-#endif
-
-typedef struct HiveClient HiveClient;
-
-typedef struct HiveClientInfo HiveClientInfo;
-
-struct HiveOAuthInfo {
-    const char *client_id;
-    const char *scope;
-    const char *redirect_url;
+    int (*get_access_token)(HiveClient *, char **access_token);
+    int (*refresh_access_token)(HiveClient *, char **access_token);
 };
 
-enum HiveDriveType {
-    HiveDriveType_Local     = 0x0,
-    HiveDriveType_OneDrive  = 0x01,
+int hive_client_get_access_token(HiveClient *, char **access_token);
 
-    HiveDriveType_ownCloud  = 0x51,
+int hive_client_refresh_access_token(HiveClient *, char **access_token);
 
-    HiveDriveType_HiveIPFS  = 0x98,
-    HiveDriveType_Butt      = 0x99
-};
-
-typedef struct HiveOptions {
-    char *persistent_location;
-    int  drive_type;
-} HiveOptions;
-typedef void HiveDriveOptions;
-
-typedef struct OneDriveOptions {
-    HiveOptions base;
-
-    const char *client_id;
-    const char *scope;
-    const char *redirect_url;
-
-    int (*grant_authorize)(const char *request_url);
-} OneDriveOptions;
-
-typedef struct OneDriveDriveOptions {
-    char drive_id[256];
-} OneDriveDriveOptions;
-
-HIVE_API
-HiveClient *hive_client_new(const HiveOptions *options);
-
-HIVE_API
-int hive_client_close(HiveClient *);
-
-HIVE_API
-int hive_client_login(HiveClient *);
-
-HIVE_API
-int hive_client_logout(HiveClient *);
-
-HIVE_API
-int hive_client_list_drives(HiveClient *, char **result);
-
-HIVE_API
-HiveDrive *hive_drive_open(HiveClient *, const HiveDriveOptions *options);
-
-HIVE_API
-int hive_client_get_info(HiveClient *, HiveClientInfo *info);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
-
-#if defined(__APPLE__)
-#pragma GCC diagnostic pop
-#endif
-
-#endif // __HIVE_CLIENT_H__
-
+#endif // __CLIENT_H__
---
+++ src/elastos_hive.h
@@ -0,0 +1,127 @@
+#ifndef __ELASTOS_HIVE_H__
+#define __ELASTOS_HIVE_H__
+
+#if defined(__APPLE__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(HIVE_STATIC)
+#define HIVE_API
+#elif defined(HIVE_DYNAMIC)
+#ifdef HIVE_BUILD
+#if defined(_WIN32) || defined(_WIN64)
+#define HIVE_API __declspec(dllexport)
+#else
+#define HIVE_API __attribute__((visibility("default")))
+#endif
+#else
+#if defined(_WIN32) || defined(_WIN64)
+      #define HIVE_API __declspec(dllimport)
+    #else
+      #define HIVE_API
+    #endif
+#endif
+#else
+#define HIVE_API
+#endif
+
+typedef struct HiveClient HiveClient;
+typedef struct HiveClientInfo HiveClientInfo;
+typedef struct HiveDrive HiveDrive;
+typedef struct HiveDriveInfo HiveDriveInfo;
+
+struct HiveOAuthInfo {
+    const char *client_id;
+    const char *scope;
+    const char *redirect_url;
+};
+
+enum HiveDriveType {
+    HiveDriveType_Local     = 0x0,
+    HiveDriveType_OneDrive  = 0x01,
+
+    HiveDriveType_ownCloud  = 0x51,
+
+    HiveDriveType_HiveIPFS  = 0x98,
+    HiveDriveType_Butt      = 0x99
+};
+
+typedef struct HiveOptions {
+    char *persistent_location;
+    int  drive_type;
+} HiveOptions;
+typedef void HiveDriveOptions;
+
+typedef struct OneDriveOptions {
+    HiveOptions base;
+
+    const char *client_id;
+    const char *scope;
+    const char *redirect_url;
+
+    int (*grant_authorize)(const char *request_url);
+} OneDriveOptions;
+
+typedef struct OneDriveDriveOptions {
+    char drive_id[256];
+} OneDriveDriveOptions;
+
+HIVE_API
+HiveClient *hive_client_new(const HiveOptions *options);
+
+HIVE_API
+int hive_client_close(HiveClient *);
+
+HIVE_API
+int hive_client_login(HiveClient *);
+
+HIVE_API
+int hive_client_logout(HiveClient *);
+
+HIVE_API
+int hive_client_get_info(HiveClient *, HiveClientInfo *info);
+
+HIVE_API
+int hive_client_list_drives(HiveClient *, char **result);
+
+HIVE_API
+HiveDrive *hive_drive_open(HiveClient *, const HiveDriveOptions *options);
+
+HIVE_API
+void hive_drive_close(HiveDrive *);
+
+HIVE_API
+int hive_drive_getinfo(HiveDrive *, HiveDriveInfo *);
+
+HIVE_API
+int hive_drive_file_stat(HiveDrive *, const char *file_path, char **result);
+
+HIVE_API
+int hive_drive_list_files(HiveDrive *, const char *dir_path, char **result);
+
+HIVE_API
+int hive_drive_mkdir(HiveDrive *, const char *path);
+
+HIVE_API
+int hive_drive_move_file(HiveDrive *, const char *old, const char *new);
+
+HIVE_API
+int hive_drive_copy_file(HiveDrive *, const char *src_path, const char *dest_path);
+
+HIVE_API
+int hive_drive_delete_file(HiveDrive *, const char *path);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#if defined(__APPLE__)
+#pragma GCC diagnostic pop
+#endif
+
+#endif // __ELASTOS_HIVE_H__
