e1b4e43b9607258a1e9b12a760278eca10ca1c44
lifenxiang <noahli2013@outlook.com>
2019-05-24 17:25:20 +0800

    implement Hive IPFS version of abstract class client

--- src/vendors/hiveipfs/hiveipfs_client.h
+++ src/vendors/hiveipfs/hiveipfs_client.h
@@ -1,16 +1,8 @@
 #ifndef __HIVE_IPFS_CLIENT_H__
 #define __HIVE_IPFS_CLIENT_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "client.h"
 
 HiveClient *hiveipfs_client_new(const HiveOptions *);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // __HIVE_IPFS_CLIENT_H__
---
+++ src/vendors/hiveipfs/hiveipfs_drive.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+#include "hiveipfs_drive.h"
+
+HiveDrive *ipfs_drive_open(HiveClient *client, const char *svr_addr, const char *uid)
+{
+    (void)client;
+    (void)svr_addr;
+    (void)uid;
+    return NULL;
+}
