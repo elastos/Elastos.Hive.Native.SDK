8fd8f5603b472f85eab03c1d771479917cfc4cbd
lifenxiang <noahli2013@outlook.com>
2019-06-13 13:06:14 +0800

    trim extraneous code

--- src/vendors/ipfs/ipfs_client.h
+++ src/vendors/ipfs/ipfs_client.h
@@ -8,15 +8,7 @@ extern "C" {
 #include "ela_hive.h"
 #include "http_client.h"
 
-typedef struct ipfs_transaction {
-    void (*setup_req)(http_client_t *, void *);
-    void *user_data;
-    http_client_t *resp;
-} ipfs_tsx_t;
-
 int ipfs_client_new(const HiveOptions *, HiveClient **);
-
-int ipfs_client_synchronize(HiveClient *);
 
 #ifdef __cplusplus
 }
