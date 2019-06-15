8e2e803f5d9fa935e50a14565467726d06fbeae9
stiartsly <stiartsly@gmail.com>
2019-06-15 15:46:00 +0800

    Move micro utility functions from error.h to ela_hive.h (#27)

--- src/ela_hive.h
+++ src/ela_hive.h
@@ -805,6 +805,16 @@ ssize_t hive_file_write(HiveFile *file, const char *buf, size_t bufsz);
  */
 #define HIVEERR_UNKNOWN                              0xFF
 
+
+#define HIVE_MK_ERROR(facility, code)  (0x80000000 | ((facility) << 24) | \
+                    ((((code) & 0x80000000) >> 8) | ((code) & 0x7FFFFFFF)))
+
+#define HIVE_GENERAL_ERROR(code)       HIVE_MK_ERROR(HIVEF_GENERAL, code)
+#define HIVE_SYS_ERROR(code)           HIVE_MK_ERROR(HIVEF_SYS, code)
+#define HIVE_HTTPC_ERROR(code)         HIVE_MK_ERROR(HIVEF_HTTP_CLIENT, code)
+#define HIVE_HTTPS_ERROR(code)         HIVE_MK_ERROR(HIVEF_HTTP_SERVER, code)
+#define HIVE_HTTP_TSX_ERROR(code)      HIVE_MK_ERROR(HIVEF_HTTP_TRANSACTION, code)
+
 /*
  * \~English
  * Retrieves the last-error code value. The last-error code is maintained on a
--- src/error.h
+++ src/error.h
@@ -1,16 +1,9 @@
-#ifndef __ERROR_H__
-#define __ERROR_H__
+#ifndef __HIVE_ERROR_H__
+#define __HIVE_ERROR_H__
 
-#include "ela_hive.h"
-
-#define HIVE_MK_ERROR(facility, code)  (0x80000000 | ((facility) << 24) | \
-                    ((((code) & 0x80000000) >> 8) | ((code) & 0x7FFFFFFF)))
-
-#define HIVE_GENERAL_ERROR(code)       HIVE_MK_ERROR(HIVEF_GENERAL, code)
-#define HIVE_SYS_ERROR(code)           HIVE_MK_ERROR(HIVEF_SYS, code)
-#define HIVE_HTTPC_ERROR(code)         HIVE_MK_ERROR(HIVEF_HTTP_CLIENT, code)
-#define HIVE_HTTPS_ERROR(code)         HIVE_MK_ERROR(HIVEF_HTTP_SERVER, code)
-#define HIVE_HTTP_TSX_ERROR(code)      HIVE_MK_ERROR(HIVEF_HTTP_TRANSACTION, code)
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void hive_set_error(int err);
 
@@ -18,4 +11,8 @@ typedef int (*strerror_t)(int errnum, char *, size_t);
 
 int hive_register_strerror(int facility, strerror_t strerr);
 
-#endif // __ERROR_H__
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // __HIVE_ERROR_H__
